efe8c2bc12f142d181b879cc32dab310c297dfc7
Donald-254 <josiahdonald@gmail.com>
2023-03-17 06:27:42 -0700

    number by putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int a = 0;
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a + '0');
+	}
+	putchar('\n');
+return (0);
+}
