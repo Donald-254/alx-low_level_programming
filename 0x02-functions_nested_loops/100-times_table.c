e08a772f79416d8ea225b198c9c15332d99dd1af
Donald-254 <josiahdonald@gmail.com>
2023-03-21 08:53:14 -0700

    multiplication

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,8 +4,7 @@
  * @n: variable
  */
 void print_times_table(int n)
-{
-	int i, j, k;
+{int i, j, k;
 
 	if (n >= 0 && n <= 15)
 	{
@@ -21,26 +20,26 @@ void print_times_table(int n)
 				if (k < 10 && j != 0)
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
 				}
 				else if (k >= 10 && k <= 99 && j != 0)
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 				}
 				else if (k >= 100 && k <= 999 && j != 0)
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k % 100) / 10) + '0');
-					_putchar((((k % 100) / 10) % 10) + '0');
+				_putchar(' ');
+				_putchar((k / 100) + '0');
+				_putchar(((k % 100) / 10) + '0');
+				_putchar(((k % 100) % 10) + '0');
 				}
 			}
 			_putchar('\n');
