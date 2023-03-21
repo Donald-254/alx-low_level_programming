920adb7ad5d64aa0925252c0210bde2edc607c99
Donald-254 <josiahdonald@gmail.com>
2023-03-21 02:08:01 -0700

    fuctions and nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point to the program
  * Return: 0 for success
@@ -10,9 +10,9 @@ int main(void)
 
 	for (i = 0; i < 8; i++)
 	{
-		putchar(a[i]);
+		_putchar(a[i]);
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
 
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print all aphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
+
