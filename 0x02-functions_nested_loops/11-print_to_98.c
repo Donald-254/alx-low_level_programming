50c5f5382166299f5c155c2cec0f2911da50ade2
Donald-254 <josiahdonald@gmail.com>
2023-03-21 07:56:51 -0700

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - natual number from or to 98
+ *@n: natural number
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%d, ", i);
+		}
+		printf("%d", 98);
+	}
+	else if (n <= 0 && n < 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+			printf("%d, ", i);
+		}
+		printf("%d", 98);
+	}
+	else if (n == 98)
+	{
+		printf("%d", 98);
+	}
+	printf("\n");
+}
