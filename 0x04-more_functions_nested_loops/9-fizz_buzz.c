b3bbf0a9d93c404c0aa0a9ad51039e6fb5974706
Donald-254 <josiahdonald@gmail.com>
2023-03-23 07:14:56 -0700

    fizz-buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,20 +12,20 @@ int main(void)
 	{
 		if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("%s ", "Fizz");
 		}
-		else if((i % 3 == 0) && (i % 5 == 0))
+		else if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf("%s ", "FizzBuzz");
 		}
 
 		else if ((i % 5 == 0) && (i != 100))
 		{
-			printf("Buzz ");
+			printf("%s ", "Buzz");
 		}
 		else if (i == 100)
 		{
-			printf("Buzz\n");
+			printf("%s\n", "Buzz");
 		}
 		else
 		{
