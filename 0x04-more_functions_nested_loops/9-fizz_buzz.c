38234f578cdf29b859ed4bf4a8d1583ec6f23f1b
Donald-254 <josiahdonald@gmail.com>
2023-03-23 06:56:49 -0700

    fizz-buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 success
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fuzz ");
+		}
+		else if ((i % 5 == 0) && (i != 100))
+		{
+			printf("Buzz ");
+		}
+		else if (i == 100)
+		{
+			printf("Buzz\n");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
