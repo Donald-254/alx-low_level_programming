8b282ef27f3a002ef0523d2bb582d0f9c0f179f3
Donald-254 <josiahdonald@gmail.com>
2023-04-10 02:37:32 -0700

    arguments passed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: arguments passed
+ * Return: success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
