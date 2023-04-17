94321df01e81abcccaf4c80bdab72219ba020467
Donald-254 <josiahdonald@gmail.com>
2023-04-17 07:56:14 -0700

    file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: arguments passed
+ * Return: success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
