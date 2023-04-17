88d5e074a89238507385ec5f03b6a2db8f49fcf2
Donald-254 <josiahdonald@gmail.com>
2023-04-17 12:01:42 -0700

    new dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,4 +14,8 @@ typedef struct dog
 } dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+int _strlen(char *s);
+char *_strcpy(char *s1, char *s2);
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif
