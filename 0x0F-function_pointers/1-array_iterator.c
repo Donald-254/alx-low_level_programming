d851cb14037e4d9f31379da890f17b4f76457443
Donald-254 <josiahdonald@gmail.com>
2023-04-19 12:33:55 -0700

    array of integers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - print each array
+ * @array: the array
+ * @size: size of array
+ * @action: action to print
+ * Return: success
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+		return;
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,8 @@
 #define FUNCTION_POINTERS_H
 void print_name(char *name, void (*f)(char *));
 #include <stdlib.h>
+void array_iterator(int *array, size_t size, void (*action)(int));
+
 
 
 
